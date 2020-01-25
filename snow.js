b17f562287d0488b399e963ae76990939fb60ded
jieun <37922134+ji-eun-k@users.noreply.github.com>
2020-01-25 18:35:07 +0900

    hi

--- snow.js
+++ snow.js
@@ -95,5 +95,5 @@ for (i = 0; i <= snowMax; i++) {
 	document.write("<span id='flake" + i + "' style='" + snowStyles + "position:absolute;top:-" + snowMaxSize + "'>" + snowEntity + "</span>");
 }
 
-window.onresize = resize;
-window.onload = initSnow;
+window.addEventListener('resize', resize);
+window.addEventListener('load', initSnow);
