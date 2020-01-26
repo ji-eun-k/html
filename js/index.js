a105563d001ce87b8b9ed8e1768bb191cda3fb21
jieun <37922134+ji-eun-k@users.noreply.github.com>
2020-01-26 10:56:33 +0900

    why

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 $('document').ready(function() {
-  var options = { videoId: 'WcrPTyiCGAk', start: 5};
-  $('#wrapper').tubular(options);
+	var options = { videoId: 'ab0TSkLe-E0', start: 3 };
+	$('#wrapper').tubular(options);
+	// f-UGhWj1xww cool sepia hd
+	// 49SKbS7Xwf4 beautiful barn sepia
 });
